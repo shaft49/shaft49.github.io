fb6785f5a6b7bea49717f4b07b8f23d63ee1ba8f
Shaafi <ishaafi2294@gmail.com>
2020-08-02 14:52:38 +0600

    Home Page added

---
+++ src/components/home.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Resume from '../documents/Md_Shafiul_Islam_Resume.pdf'
+
+const Home = () => {
+    return (
+        <div>
+        <section id="template-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Hi! <br />I'm Shafiul</h1>
+                          <p><a className="btn btn-primary btn-learn" href={Resume} target="_blank" rel="noopener noreferrer">View Resume <i className="icon-document" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I love building<br /> THINGS !!</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/shaft49" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I often <br/>Write ... </h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://medium.com/@md.shafiul.islam.ofcl" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
+};
+
+export default Home;
--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Sidebar from '../components/sidebar'
+import Home from '../components/home'
 
 function Main() {
     return(
         <div id = "template-page">
             <div id = "container-wrap">
                 <Sidebar></Sidebar>
+                <div id = "template-main">
+                <Home></Home>
+                </div>
             </div>
         </div>
     );
